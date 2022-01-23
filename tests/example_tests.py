eca008440d55c087a3dd5a2ef6d8a41a51b0a0f9
Abhishek Sharma <online.abhisheksharma@gmail.com>
2022-01-24 02:06:34 +0530

    update tests

--- tests/example_tests.py
+++ tests/example_tests.py
@@ -6,11 +6,11 @@ def test_check_type():
     assert type(int_var) == int
 
 @pytest.mark.check_class
-def test_check_type():
+def test_check_type2():
     assert True
 
 @pytest.mark.e2e_test
-def test_scenario2():
+def test_scenario1():
     assert True
 
 @pytest.mark.e2e_test
